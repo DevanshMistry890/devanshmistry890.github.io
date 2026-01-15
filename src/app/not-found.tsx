d6b813fb3772bcf3cc9c94903afab5af7f65fdd6
DevanshMistry890 <devanshmistry890@gmail.com>
2026-01-15 16:09:36 -0500

    Fix build errors: remove dynamic 404 route

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Error from "@/components/error";
+import Wrapper from "@/layouts/Wrapper";
+
+export const metadata = {
+    title: "404 error || Devansh Mistry",
+};
+
+const NotFound = () => {
+    return (
+        <Wrapper>
+            <Error />
+        </Wrapper>
+    );
+};
+
+export default NotFound;
